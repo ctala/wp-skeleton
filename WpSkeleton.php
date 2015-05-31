f9c2465525edb3e547321a1b31b6283d35c817eb
Cristian Tala <cristian.tala@gmail.com>
2015-05-31 18:26:29 -0300

    AGREGANDO ARCHIVO PLUGIN

---
+++ WpSkeleton.php
@@ -0,0 +1,12 @@
+/*
+Plugin Name: Mi Plugin
+Plugin URI:  https://github.com/ctala/wp-skeleton
+Description: Base para la Creación de otros plugins
+Version:     1.0
+Author:      Cristian Tala Sánchez
+Author URI:  http://www.cristiantala.cl
+License:     MIT
+License URI: http://opensource.org/licenses/MIT
+Domain Path: /languages
+Text Domain: ctala-context
+*/
