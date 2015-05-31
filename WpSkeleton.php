eb6efb61c35aa5738b1c869e0bff3c169603212a
Cristian Tala <cristian.tala@gmail.com>
2015-05-31 18:33:07 -0300

    Agregando Menús a la Base

--- WpSkeleton.php
+++ WpSkeleton.php
@@ -1,3 +1,5 @@
+<?php
+
 /*
 Plugin Name: Mi Plugin
 Plugin URI:  https://github.com/ctala/wp-skeleton
@@ -8,5 +10,22 @@ Author URI:  http://www.cristiantala.cl
 License:     MIT
 License URI: http://opensource.org/licenses/MIT
 Domain Path: /languages
-Text Domain: ctala-context
+Text Domain: ctala-text_domain
 */
+
+// Register Navigation Menus
+function custom_navigation_menus() {
+
+	$locations = array(
+		'Herramientas' => __( 'Herramientas Básicas', 'ctala-text_domain' ),
+		'Configuración' => __( 'Configurar el plugin', 'ctala-text_domain' ),
+	);
+	register_nav_menus( $locations );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'custom_navigation_menus' );
+
+
+?>
