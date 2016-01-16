689006125ba51ac99ade93b41e1c491e57c99cd4
Cristian Tala S. <cristian.tala@gmail.com>
2016-01-16 10:17:01 -0300

    Creando Modo Esqueleto para Proyectos

--- src/MyClass.php
+++ src/MyClass.php
@@ -40,4 +40,19 @@ class MyClass {
         echo "HOLA\n";
     }
 
+    /**
+     * A method that will read a file, run a strtr to replace placeholders with
+     * values from our replace array and write it back to the file.
+     *
+     * @param string $target the filename of the target
+     * @param array $replaces the replaces to be applied to this target
+     */
+    function applyValues($target, $replaces) {
+        file_put_contents(
+                $target, strtr(
+                        file_get_contents($target), $replaces
+                )
+        );
+    }
+
 }
